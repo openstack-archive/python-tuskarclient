af224af2ab5ad31443ea5294f8c714e2d721be6a
Jiri Stransky <jistr@redhat.com>
2013-07-04 16:32:33 +0200

    Add API client code from ceilometerclient
    Contains code for making http requests as well as very basic foundation
    classes for resources and their managers.
    
    Also adds FakeAPI and FakeResponse classes to test utils.

--- tuskarclient/tests/utils.py
+++ tuskarclient/tests/utils.py
@@ -10,9 +10,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import copy
 import fixtures
 import os
+import StringIO
 import testtools
+
+from tuskarclient.common import http
 
 
 class TestCase(testtools.TestCase):
@@ -27,3 +31,41 @@ class TestCase(testtools.TestCase):
                 os.environ.get('OS_STDERR_CAPTURE') == '1'):
             stderr = self.useFixture(fixtures.StringStream('stderr')).stream
             self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
+
+
+class FakeAPI(object):
+    def __init__(self, fixtures):
+        self.fixtures = fixtures
+        self.calls = []
+
+    def _request(self, method, url, headers=None, body=None):
+        call = (method, url, headers or {}, body)
+        self.calls.append(call)
+        return self.fixtures[url][method]
+
+    def raw_request(self, *args, **kwargs):
+        fixture = self._request(*args, **kwargs)
+        body_iter = http.ResponseBodyIterator(StringIO.StringIO(fixture[1]))
+        return FakeResponse(fixture[0]), body_iter
+
+    def json_request(self, *args, **kwargs):
+        fixture = self._request(*args, **kwargs)
+        return FakeResponse(fixture[0]), fixture[1]
+
+
+class FakeResponse(object):
+    def __init__(self, headers, body=None, version=None):
+        """:param headers: dict representing HTTP response headers
+        :param body: file-like object
+        """
+        self.headers = headers
+        self.body = body
+
+    def getheaders(self):
+        return copy.deepcopy(self.headers).items()
+
+    def getheader(self, key, default):
+        return self.headers.get(key, default)
+
+    def read(self, amt):
+        return self.body.read(amt)
