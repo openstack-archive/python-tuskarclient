6059b2c446a94a2c9db2f7f9f4521cee0c6201bb
Petr Blaho <pblaho@redhat.com>
2013-08-14 15:12:31 +0200

    Adds test for tuskarclient/client._get_ksclient()
    Change-Id: I56c5d8f1ba225b17b7494a8897e92c0730427589

--- tuskarclient/tests/test_client.py
+++ tuskarclient/tests/test_client.py
@@ -46,3 +46,44 @@ class ClientGetEndpointTest(tutils.TestCase):
             service_type='service_type_value',
             endpoint_type='endpoint_type_value',
         )
+
+
+class ClientGetKSClientTest(tutils.TestCase):
+
+    @mock.patch.object(client.ksclient, 'Client')
+    def test_selects_proper_values(self, mocked_ksclient):
+        mocked_ksclient.return_value = 'mocked ksclient'
+        kwargs = {}
+        kskeys = {  # keys for KeystoneClient constructor
+            'username',
+            'password',
+            'tenant_id',
+            'tenant_name',
+            'auth_url',
+            'insecure',
+        }
+        redundant_keys = {  # key added to see if not passed to constructor
+            'some_other_key',
+            'any_other_key',
+        }
+        missing_keys = {  # keys missing to see if its value defaults to None
+            'username',
+            'auth_url',
+        }
+
+        # contruct testing dictionary with redunant keys and missing some
+        for key in kskeys | redundant_keys:
+            if key not in missing_keys:
+                kwargs[key] = key + '_value'
+
+        # construct dict containing expected values - no reduntant keys and
+        # default for missing keys
+        ksargs = kwargs.copy()
+        for key in redundant_keys:
+            del ksargs[key]
+
+        for key in missing_keys:
+            ksargs[key] = None
+
+        self.assertEqual(client._get_ksclient(**kwargs), 'mocked ksclient')
+        mocked_ksclient.assert_called_with(**ksargs)
