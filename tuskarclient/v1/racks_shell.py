820fe7440af7f0763ada3e09c2d21028534cea7e
Jiri Stransky <jistr@redhat.com>
2013-08-26 17:54:54 +0200

    Racks CRUD commands
    Commands implemented previously and tweaked:
    rack-show
    rack-list
    
    Newly implemented commands:
    rack-create
    rack-update
    rack-delete
    
    We might eventually want to provide a way to set capacities for racks
    and flavors one by one, but after discussing with multiple people we
    still don't have obvious correct way to do this, so i'm leaving it out
    for now and it can be added later. Currently we can set capacities in
    bulk via --capacities.
    
    Partially implements
    https://blueprints.launchpad.net/python-tuskarclient/+spec/racks-cli
    
    Change-Id: Ic45f3c157e56b976438c9559245858c831511797

--- tuskarclient/v1/racks_shell.py
+++ tuskarclient/v1/racks_shell.py
@@ -10,18 +10,84 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from __future__ import print_function
+
 import tuskarclient.common.formatting as fmt
 from tuskarclient.common import utils
 from tuskarclient import exc
 
 
-# TODO(jistr): This is PoC, not final implementation
 @utils.arg('id', metavar="<NAME or ID>", help="Name or ID of rack to show.")
 def do_rack_show(tuskar, args):
-    try:
-        rack = utils.find_resource(tuskar.racks, args.id)
-    except exc.HTTPNotFound:
-        raise exc.CommandError("Rack not found: %s" % args.id)
+    rack = utils.find_resource(tuskar.racks, args.id)
+    print_rack_detail(rack)
+
+
+def do_rack_list(tuskar, args):
+    racks = tuskar.racks.list()
+    fields = ['id', 'name', 'subnet', 'state', 'nodes']
+    labels = {'nodes': '# of nodes'}
+    formatters = {'nodes': len}
+    fmt.print_list(racks, fields, formatters, labels)
+
+
+@utils.arg('name', help="Name of the rack to create.")
+@utils.arg('--subnet', required=True,
+           help="Rack's network in IP/CIDR notation.")
+@utils.arg('--slots', required=True, help="Number of slots in the rack.")
+@utils.arg('--capacities', help="Total capacities of the rack.")
+@utils.arg('--resource-class', help="Resource class to assign the rack to.")
+def do_rack_create(tuskar, args):
+    rack_dict = create_rack_dict(args)
+    rack = tuskar.racks.create(**rack_dict)
+    print_rack_detail(rack)
+
+
+@utils.arg('id', metavar="<NAME or ID>", help="Name or ID of rack to show.")
+@utils.arg('--name', help="Rack's updated name.")
+@utils.arg('--subnet', help="Rack's network in IP/CIDR notation.")
+@utils.arg('--capacities', help="Total capacities of the rack.")
+@utils.arg('--slots', help="Number of slots in the rack.")
+@utils.arg('--resource-class', help="Resource class to assign the rack to.")
+def do_rack_update(tuskar, args):
+    rack = utils.find_resource(tuskar.racks, args.id)
+    rack_dict = create_rack_dict(args)
+    updated_rack = tuskar.racks.update(rack.id, **rack_dict)
+    print_rack_detail(updated_rack)
+
+
+@utils.arg('id', metavar="<NAME or ID>", help="Name or ID of rack to show.")
+def do_rack_delete(tuskar, args):
+    rack = utils.find_resource(tuskar.racks, args.id)
+    tuskar.racks.delete(args.id)
+    print('Deleted rack "%s".' % rack.name)
+
+
+def create_rack_dict(args):
+    '''Create a rack dict for use with API requests from supplied command
+    line args.
+    '''
+    rack_dict = {}
+    simple_fields = ['name', 'subnet', 'slots']
+    for field_name in simple_fields:
+        field_value = vars(args)[field_name]
+        if field_value is not None:
+            rack_dict[field_name] = field_value
+
+    if args.resource_class == '':
+        rack_dict['resource_class'] = None
+    elif args.resource_class:
+        # TODO(jistr): support for selecting resource class by name
+        rack_dict['resource_class'] = {'id': args.resource_class}
+
+    if args.capacities is not None:
+        rack_dict['capacities'] = parse_capacities(args.capacities)
+
+    return rack_dict
+
+
+def print_rack_detail(rack):
+    '''Print detailed rack information (for rack-show etc.).'''
     formatters = {
         'capacities': fmt.capacities_formatter,
         'chassis': fmt.resource_link_formatter,
@@ -39,11 +105,25 @@ def do_rack_show(tuskar, args):
     fmt.print_dict(rack_dict, formatters)
 
 
-# TODO(jistr): This is PoC, not final implementation
-def do_rack_list(tuskar, args):
-    racks = tuskar.racks.list()
-    fields = ['id', 'name', 'subnet', 'state', 'nodes']
-    labels = {'nodes': '# of nodes'}
-    formatters = {'nodes': len}
+def parse_capacities(capacities_str):
+    '''Take capacities from CLI and parse them into format for API.
 
-    fmt.print_list(racks, fields, formatters, labels)
+    :param capacities_string: string of capacities like
+        'total_cpu:64:CPU,total_memory:1024:MB'
+    :return: array of capacities dicts usable for requests to API
+    '''
+    if capacities_str == '':
+        return []
+
+    capacities = []
+    for capacity_str in capacities_str.split(','):
+        fields = capacity_str.split(':')
+        if len(fields) != 3:
+            raise exc.CommandError(
+                'Capacity info "{0}" should be 3 fields separated by colons. '
+                '(Use commas to separate multiple capacities.)'
+                .format(capacity_str))
+        capacities.append(
+            {'name': fields[0], 'value': fields[1], 'unit': fields[2]})
+
+    return capacities
