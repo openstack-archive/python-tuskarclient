1e00b3cd1b03abae2ea6011f8c689c92689cc13a
Petr Blaho <pblaho@redhat.com>
2013-11-13 18:22:01 +0100

    Adds help for subcommands
    Adds help subcommand which takes other subcommands as param.
    Adds -h/--help option for subcommands.
    
    This contains integration testing code for tuskar CLI.
    Following tests are implemented:
    o bare help command
    o help command with bad parameter
    o help command with -h / --help parameter
    o set of tests for "help <command>", "<command> -h / --help" for following
    commands:
      - flavor-create
      - flavor-delete
      - flavor-list
      - flavor-show
      - flavor-update
      - rack-create
      - rack-delete
      - rack-list
      - rack-show
      - rack-update
    
    Fixes bug in tests/test_shell.py - {} -> []
    
    Fixes bug: 1213050
    
    Change-Id: Ic554198f4fb4cdbaeefb9831c15f969301a7be87

---
+++ tuskarclient/tests/integration/test_help_flavor_update.py
@@ -0,0 +1,50 @@
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import tuskarclient.tests.utils as tutils
+
+
+class HelpFlavorUpdateTest(tutils.CommandTestCase):
+    def test_flavor_update_with_h(self):
+        self.help_for_flavor_update_test('flavor-update -h')
+
+    def test_flavor_update_with_help(self):
+        self.help_for_flavor_update_test('flavor-update --help')
+
+    def test_help_flavor_create(self):
+        self.help_for_flavor_update_test('help flavor-update')
+
+    def help_for_flavor_update_test(self, command):
+        results = self.run_tuskar(command)
+        out_includes = [
+            'usage: tuskar flavor-update [-h] [--name NAME]'
+            + ' [--capacities CAPACITIES]',
+            'positional arguments:',
+            'optional arguments:',
+            '--capacities CAPACITIES',
+            '--name NAME',
+            '--max-vms MAX_VMS',
+        ]
+        out_excludes = [
+            'flavor-list',
+            '--os-username OS_USERNAME',
+            'Display help for <subcommand>',
+        ]
+        self.assertThat(
+            results,
+            tutils.CommandOutput(
+                out_inc=out_includes,
+                out_exc=out_excludes,
+                err_str='',
+                return_code=0,
+            ))
---
+++ tuskarclient/tests/integration/test_help_rack_update.py
@@ -0,0 +1,52 @@
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import tuskarclient.tests.utils as tutils
+
+
+class HelpRackUpdateTest(tutils.CommandTestCase):
+    def test_rack_update_with_h(self):
+        self.help_for_rack_update_test('rack-update -h')
+
+    def test_rack_update_with_help(self):
+        self.help_for_rack_update_test('rack-update --help')
+
+    def test_help_rack_create(self):
+        self.help_for_rack_update_test('help rack-update')
+
+    def help_for_rack_update_test(self, command):
+        results = self.run_tuskar(command)
+        out_includes = [
+            'usage: tuskar rack-update [-h] [--name NAME] [--subnet SUBNET]',
+            'positional arguments:',
+            '<NAME or ID>',
+            'optional arguments:',
+            '--name NAME',
+            '--subnet SUBNET',
+            '--slots SLOTS',
+            '--capacities CAPACITIES',
+            '--resource-class RESOURCE_CLASS',
+        ]
+        out_excludes = [
+            'rack-list',
+            '--os-username OS_USERNAME',
+            'Display help for <subcommand>',
+        ]
+        self.assertThat(
+            results,
+            tutils.CommandOutput(
+                out_inc=out_includes,
+                out_exc=out_excludes,
+                err_str='',
+                return_code=0,
+            ))
