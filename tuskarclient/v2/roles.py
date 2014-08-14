27638a340a7862bdb9ca9b1d97c9fb711a95795a
Petr Blaho <pblaho@redhat.com>
2014-08-14 13:58:04 +0200

    Adds empty RoleManager to v2 client
    Adds RoleManager and Role resource
    Adds RoleManager to v2 client
    Adds _path() to RoleManager
    Adds basic tests
    
    Change-Id: Id903d6e9469d62c5fe566dbdf3d55be8b45a75f8

---
+++ tuskarclient/v2/roles.py
@@ -0,0 +1,40 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from tuskarclient.common import base
+from tuskarclient.openstack.common.apiclient import base as common_base
+
+
+class Role(common_base.Resource):
+    """Represents an instance of a Role in the Tuskar API.
+
+    :param manager: Manager object
+    :param info: dictionary representing resource attributes
+    :param loaded: prevent lazy-loading if set to True
+    """
+
+
+class RoleManager(base.Manager):
+    """RoleManager interacts with the Tuskar API and provides
+    operations for adding/removing Roles to/from Plans.
+    """
+
+    # The class used to represent a Role instance
+    resource_class = Role
+
+    @staticmethod
+    def _path(role_id=None):
+
+        if role_id:
+            return '/v2/roles/%s' % role_id
+
+        return '/v2/roles'
