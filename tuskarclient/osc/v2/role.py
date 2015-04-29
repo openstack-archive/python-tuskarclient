fa8f03070da6c7277ec237d511acc7686782be71
Dougal Matthews <dougal@redhat.com>
2015-04-29 14:27:14 +0100

    Add stub commands and tests for all OpenStack client commands
    This adds the overall stubs for all the Tuskar commands that will be
    added to the OpenStack client.
    
    Change-Id: Ia3d465f253aedb5825a9db552b560e50945c9bf7

---
+++ tuskarclient/osc/v2/role.py
@@ -0,0 +1,28 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+import logging
+
+from cliff import lister
+
+
+class ListRoles(lister.Lister):
+    """List Roles."""
+
+    log = logging.getLogger(__name__ + '.ListRoles')
+
+    def get_parser(self, prog_name):
+        parser = super(ListRoles, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)" % parsed_args)
